d3ab6cff73be0c3e43a2b8e1e00fa089b8f83d0c
prashanthPadmasahli <prashanthkumarshettigar@gmail.com>
2023-12-30 17:38:17 +0530

    Initial Commit

---
+++ Pagination/PaginationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaginationApp.swift
+//  Pagination
+//
+//  Created by mac on 30/12/23.
+//
+
+import SwiftUI
+
+@main
+struct PaginationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
